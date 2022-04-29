8df46f5f72c3738777f66d6fde027895d6a6ec10
Andrew Leck <Aleck360@gmail.com>
2022-04-28 23:35:52 -0500

    completed two functions and tested to make sure they work

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,24 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket=[];
+console.log(basket);
+function addItem(item){
+    basket.push(item);
+    return true;
+}
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem(' apples '));
+console.log('adding pizza  (expect true)', addItem(' pizza '))
+console.log('adding popcorn  (expect true)', addItem(' popcorn '))
+console.log('adding chicken  (expect true)', addItem('chicken'))
+console.log(`Basket is now ${basket}`);
+
+function listItems(basket){
+ for(let i=0; i< basket.length; i++){
+    console.log(basket[i]);   
+ }
+}
+
+console.log(listItems(basket));
